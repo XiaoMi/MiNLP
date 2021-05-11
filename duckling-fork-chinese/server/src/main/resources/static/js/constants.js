50ee9e351cb05fbb9c411cb4ea0049fbd8a76e2d
zhangsonglei <zhangsonglei@xiaomi.com>
2021-05-11 14:05:33 +0800

    [DUCK]补充readme，删除无效代码，修改js引用方式

--- duckling-fork-chinese/server/src/main/resources/static/js/constants.js
+++ duckling-fork-chinese/server/src/main/resources/static/js/constants.js
@@ -1,11 +1,5 @@
 let duckling_addr="/duckling/";
-
-//let allDims = "listDims";
 let extractInfo="extract";
-
-//function getDimsInfoUrl() {
-//    return duckling_addr + allDims;
-//}
 
 function getExtractInfoUrl() {
     return duckling_addr + extractInfo;
