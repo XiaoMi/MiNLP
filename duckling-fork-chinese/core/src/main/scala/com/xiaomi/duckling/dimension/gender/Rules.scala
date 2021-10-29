8b497519a10f1c69b04fad91fc729877b797d0da
Ninglin Du <duninglin@xiaomi.com>
2021-10-29 17:24:34 +0800

    更多性别识别

--- duckling-fork-chinese/core/src/main/scala/com/xiaomi/duckling/dimension/gender/Rules.scala
+++ duckling-fork-chinese/core/src/main/scala/com/xiaomi/duckling/dimension/gender/Rules.scala
@@ -31,9 +31,11 @@ trait Rules extends DimRules {
     builder.put("女", "女")
     builder.put("女性", "女")
     builder.put("女生", "女")
+    builder.put("女孩", "女")
     builder.put("男", "男")
     builder.put("男性", "男")
     builder.put("男生", "男")
+    builder.put("男孩", "男")
 
     Dict(vocab = builder.build())
   }
